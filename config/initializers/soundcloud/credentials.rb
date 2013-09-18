80d310ddc79208142045e146178fd7e6f7b165e4
Morgan Brown <brown.mhg@gmail.com>
2013-09-18 16:10:14 -0400

    Create config infrastructure for soundcloud

---
+++ config/initializers/soundcloud/credentials.rb
@@ -0,0 +1,2 @@
+SoundcloudQueue::Application.config.soundcloud_client_id = ENV['SOUNDCLOUD_CLIENT_ID']
+SoundcloudQueue::Application.config.soundcloud_client_secret = ENV['SOUNDCLOUD_CLIENT_SECRET']
