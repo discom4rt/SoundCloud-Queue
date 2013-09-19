ce712c2995a939c80a316120b713f56be5bdd1dc
Morgan Brown <brown.mhg@gmail.com>
2013-09-19 13:52:14 -0400

    Fix route to stream controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ SoundcloudQueue::Application.routes.draw do
   resources :sessions, :only => [:new] do
     get 'create', :on => :collection
   end
-  resource :stream, :only => [:show]
+
+  resource :stream, :only => [:show], :controller => :stream
+
   root 'application#index'
 end
