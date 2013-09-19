7069de80afa717d3a60f4bbfbf1fa2e85bd9ac34
Morgan Brown <brown.mhg@gmail.com>
2013-09-19 14:20:35 -0400

    And get rid of the filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate
-
   def index
     redirect_to stream_url if session[SessionsController::SC_TOKEN_KEY]
   end
