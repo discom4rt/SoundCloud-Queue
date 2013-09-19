6d40ac394b9c2a8a9fd64a93c26d479a48f4239f
Morgan Brown <brown.mhg@gmail.com>
2013-09-19 13:01:02 -0400

    Set up simple stream

---
+++ app/controllers/stream_controller.rb
@@ -0,0 +1,19 @@
+class StreamController < ApplicationController
+
+  before_filter :init_user, :init_soundcloud
+
+  def show
+    @current_user = @client.get('/me')
+  end
+
+  private
+
+  def init_user
+    @user = User.where(:access_token => session[SC_TOKEN_KEY]).first
+  end
+
+  def init_soundcloud
+    @client = Soundcloud.new(:access_token => @user.access_token)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ SoundcloudQueue::Application.routes.draw do
   resources :sessions, :only => [:new] do
     get 'create', :on => :collection
   end
+  resource :stream, :only => [:show]
   root 'application#index'
 end
