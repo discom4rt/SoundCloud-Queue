d3325683846323fa1e0dac7ff1cbbd34bf9811a1
Morgan Brown <brown.mhg@gmail.com>
2013-10-16 17:13:22 +0200

    Add ability to get all of a user's stream

--- app/assets/javascripts/stream.js
+++ app/assets/javascripts/stream.js
@@ -4,8 +4,10 @@ SC = SC || {};
 SC.Stream = function( $container ) {
   this.$container = $container;
   this.$tracks = this.$container.find('iframe');
-  this.setupContinuousPlay();
+  this.isLoadingNext = false;
+  this.setupContinuousPlay( this.$tracks );
   this.setupQueuing();
+  this.setupInfiniteScroll();
 };
 
 SC.Stream.prototype.setupQueuing = function() {
@@ -16,8 +18,25 @@ SC.Stream.prototype.setupQueuing = function() {
   });
 };
 
-SC.Stream.prototype.setupContinuousPlay = function() {
-  this.$tracks.each(function() {
+SC.Stream.prototype.setupContinuousPlay = function( $tracks ) {
+  var $first = $($tracks[0]),
+   $previous = $first.closest('li').prev().find('iframe'),
+   firstWidget,
+   previousWidget;
+
+  // Play when last section's last song completes
+  if( $previous.length && $first.length ) {
+    firstWidget = SC.Widget( $first[0] );
+    previousWidget = SC.Widget( $previous[0] );
+
+    firstWidget.bind(SC.Widget.Events.READY, function() {
+      previousWidget.bind(SC.Widget.Events.FINISH, function() {
+        firstWidget.play();
+      });
+    });
+  }
+
+  $tracks.each(function() {
     var $widgetIframe = $(this),
       widget = SC.Widget( $widgetIframe[0] );
 
@@ -30,6 +49,29 @@ SC.Stream.prototype.setupContinuousPlay = function() {
       });
     });
   });
+};
+
+SC.Stream.prototype.setupInfiniteScroll = function() {
+  $(window).on( 'scroll', $.proxy( this.loadNext, this ) );
+};
+
+SC.Stream.prototype.loadNext = function( event ) {
+  var self = this,
+    $nextItem;
+
+  if ( $(window).scrollTop() + $(window).height() > $(document).height() - 200 && !this.isLoadingNext ) {
+    this.isLoadingNext = true;
+    $nextItem = this.$container.find('.next');
+    $nextItem.show();
+
+    $.get('stream?next=' + $nextItem.data('next'), function( data ) {
+      var $trackItems = $(data);
+
+      $nextItem.replaceWith( $trackItems );
+      self.setupContinuousPlay( $trackItems.find( 'iframe' ) );
+      self.isLoadingNext = false;
+    });
+  }
 };
 
 $(function(){
--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -2,9 +2,17 @@ class StreamController < ApplicationController
 
   before_filter :authenticate, :init_user, :init_soundcloud
 
+  SC_NEXT_KEY = 'next'
+
   def show
     @current_user = @client.get('/me')
-    @stream = @client.get('/me/activities')
+
+    if params[:next]
+      @stream = @client.get(params[:next])
+      render :partial => "tracks", :layout => false and return
+    else
+      @stream = @client.get('/me/activities')
+    end
   end
 
   private
